15be8e4b5ec3e31cdd07a3440a7784fc7de5beff
btanqy <btanqy@gmail.com>
2025-01-04 08:57:29 -0800

    php

--- Main/banner.php
+++ Main/banner.php
@@ -2,12 +2,12 @@
 
 <div class="banner inline no-decoration">
   <div class="banner-content left-text">
-    <a href="/Blog/index.php"> btanqy </a>
+    <a href="<?php echo BASE; ?>index.php"> btanqy </a>
   </div>
   <div class="right-text body-black banner-link">
-    <a href="/Blog/index.php">Home</a> &nbsp;
-    <a href="/Blog/Main/Projects/Overview/overview.php">Projects</a> &nbsp;
-    <a href="/Blog/Main/Blog/Overview/overview.php">Blog</a>
-    &nbsp; <a href="/Blog/Main/About/about.php">About</a>
+    <a href="<?php echo BASE; ?>index.php">Home</a> &nbsp;
+    <a href="<?php echo BASE; ?>/Main/Projects/Overview/overview.php">Projects</a> &nbsp;
+    <a href="<?php echo BASE; ?>/Main/Blog/Overview/overview.php">Blog</a>
+    &nbsp; <a href="<?php echo BASE; ?>/Main/About/about.php">About</a>
   </div>
 </div>
--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 // Define the project root directory
 define('ROOT', dirname(__FILE__));
-define('BASE', "/");
+define('BASE', "http://btanqy.unaux.com/");
 ?>
