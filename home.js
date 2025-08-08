0e47a6fff26f95f5859af82de66d6dfc4bcae2e2
btanqy <btanqy@gmail.com>
2025-08-08 15:23:06 -0700

    test

--- home.js
+++ home.js
@@ -22,16 +22,17 @@ fetch('posts.json')
       div.onclick = () => window.location.href = post.url;
 
       div.innerHTML = `
-        <div class="bottom-left">
-          <p>${post.title}</p>
-          <div class="bottom-left-small">
-            <p>${post.preview}</p>
-          </div>
-          <div class="bottom-right">
-            <p>${post.date}</p>
-          </div>
+      <div class="bottom-left">
+        <p>${post.title}</p>
+        <div class="bottom-left-small">
+          <p>${post.preview}</p>
         </div>
-      `;
+      </div>
+      <div class="bottom-right">
+        <p>${post.date}</p>
+      </div>
+    `;
+
       wrap.appendChild(div);
     });
   })
